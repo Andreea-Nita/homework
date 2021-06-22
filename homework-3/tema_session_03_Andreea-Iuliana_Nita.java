d92afce2982a0eef4613d4f6c5602cf1e2fe8338
Andreea Nita <85620337+Andreea-Nita@users.noreply.github.com>
2021-06-22 15:35:07 +0300

    Update tema_session_03_Andreea-Iuliana_Nita.java

--- homework-3/tema_session_03_Andreea-Iuliana_Nita.java
+++ homework-3/tema_session_03_Andreea-Iuliana_Nita.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class homework3 {
+    public static void main(String[] args) {
+        greeting();
+
+    }
+    public static void greeting() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Type in your name (or just press Enter if you want to be anonymous): ");
+        String name= scanner.nextLine();
+        if (name.length()==0) {
+            System.out.println("Hello, stranger!");
+        } else {
+            System.out.println("Hello, "+name+"!");
+        }
+    }
+
+}
 
